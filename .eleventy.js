7d0f90d36fa44f0520e9b8703f0ade2e65ed6674
Viktor Vasilev <viktor_vv@abv.bg>
2024-03-31 23:56:10 +0200

    designed the individual lecture page

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/styles");
     eleventyConfig.addPassthroughCopy("./src/assets");
-    eleventyConfig.addPassthroughCopy("./src/lectures");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
 
     return {
         dir: {
