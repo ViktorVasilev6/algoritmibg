f59865a7348da79164ad5fda361117070ada85e6
ViktorVasilev6 <viktor_vv@abv.bg>
2024-05-01 23:16:29 +0200

    added tailwind css and changed the whole design

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,njk,js}",
+  '*.{html,js}'],
+  theme: {
+    extend: {
+      typography: {
+        xl: {
+          css: {
+            em: {
+              'display': 'block',
+              'text-align': 'center',
+            },
+            pre: {
+              'padding': 0,
+              'line-height': '1.5rem',
+            },
+            code: {
+              'border-radius': '1rem',
+              '--tw-shadow': 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
+              '--tw-shadow-colored': 'inset 0 2px 4px 0 var(--tw-shadow-color)',
+              'box-shadow': 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
+            },
+            p: {
+              'text-align': 'justify',
+            },
+            ul: {
+              'padding-left': 0
+            },
+            h1: {
+              'text-align': 'center'
+            }
+          },
+        },
+        base: {
+          css: {
+            em: {
+              'display': 'block',
+              'text-align': 'center',
+            },
+            pre: {
+              'padding': 0,
+              'line-height': '1.5rem',
+              'font-size': '1rem'
+            },
+            code: {
+              'border-radius': '1rem',
+              '--tw-shadow': 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
+              '--tw-shadow-colored': 'inset 0 2px 4px 0 var(--tw-shadow-color)',
+              'box-shadow': 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
+            },
+            h1: {
+              'text-align': 'center'
+            }
+      },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
+
